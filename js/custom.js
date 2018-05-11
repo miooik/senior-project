eeb3b3b2598ab1cb419b1225148063c7e737574b
mookmanee <miooik2828@gmail.com>
2018-05-11 16:01:56 +0700

    ลบ slide ยังไม่ใส่คำ

--- js/custom.js
+++ js/custom.js
@@ -70,3 +70,38 @@ var animation5 = bodymovin.loadAnimation({
   path: "/js/animate/proflie/proflie.json",
 })
 
+
+var animation6 = bodymovin.loadAnimation({
+  container: document.getElementById('bedroom'),
+  renderer: 'svg',
+  loop: false,
+  autoplay: true,
+  path: "/js/animate/bedroom/bedroom.json",
+})
+
+// Get the modal
+var myModalBedRoom = document.getElementById('myModalBedRoom');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var btnSunBedRoom = document.getElementById('btnSunBedRoom');
+var modalImg = document.getElementById("myimg01");
+var captionText = document.getElementById("caption");
+
+btnSunBedRoom.onclick = function(){
+  myModalBedRoom.style.display = "block";
+  modalImg.src = "image/bedroomday.jpg";
+  var mynavbardesktop = document.getElementById('mynavbardesktop')
+  mynavbardesktop.style.display = "none";
+  var mynavbarmobile = document.getElementById('mynavbarmobile')
+  mynavbarmobile.style.display = "none";
+}
+
+// Get the <span> element that closes the modal
+var spanCloseBedRoom = document.getElementById("spanCloseBedRoom");
+
+// When the user clicks on <span> (x), close the modal
+spanCloseBedRoom.onclick = function() { 
+  var mynavbardesktop = document.getElementById('mynavbardesktop')
+  mynavbardesktop.style.display = "inherit";
+  myModalBedRoom.style.display = "none"
+}
